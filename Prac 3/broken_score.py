f5803dd86344ffbbbb9c0a108d18476e55309c4d
ChrisReppel <42366521+ChrisReppel@users.noreply.github.com>
2019-04-02 18:23:25 +1000

    Prac 3

---
+++ Prac 3/broken_score.py
@@ -0,0 +1,24 @@
+"""
+Chris Reppel
+Prac 3
+19/3/19
+"""
+
+
+def main():
+    score = float(input("Enter your score: "))
+    print(determine_status(score))
+
+
+def determine_status(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
