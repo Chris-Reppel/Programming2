8fa3e3997308c5742e5ddecd5caeadbb17eb21b9
ChrisReppel <42366521+ChrisReppel@users.noreply.github.com>
2019-04-02 18:35:10 +1000

    word occurrences done

--- Prac 5/word_occurances.py
+++ Prac 5/word_occurances.py
@@ -4,4 +4,16 @@ Prac 5
 2/4/19
 """
 
-words = {}
+count_words = {}
+text = input("Text: ")
+words = text.split()
+for word in words:
+    occurrence = count_words.get(word, 0)
+    count_words[word] = occurrence + 1
+
+words = list(count_words.keys())
+words.sort()
+
+length = max((len(word) for word in words))
+for word in words:
+    print("{:{}} : {}".format(word, length, count_words[word]))
